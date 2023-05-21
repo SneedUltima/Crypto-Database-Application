5dc07dec577625a9e9ec9f0ae18e759c459f815d
Dylan <dylan.cobham@yahoo.com.au>
2023-05-21 22:58:28 +0930

    build: add and style coinsearch and coinitem components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
+import { Route, Routes } from "react-router-dom";
 import Navbar from "./Components/Navbar";
+import Home from "./Pages/Home";
+import SignIn from "./Pages/SignIn";
+import Signup from "./Pages/Signup";
+import Account from "./Pages/Account";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [coins, setCoints] = useState([]);
+
+  const url =
+    "https://api.coingecko.com/api/v3/coins/markets?vs_currency=aud&order=market_cap_desc&per_page=10&page=1&sparkline=true&locale=en";
+
+  useEffect(() => {
+    axios.get(url).then((response) => {
+      setCoints(response.data);
+    });
+  }, []);
+
   return (
     <div className="App bg-crypto-blue-light w-full h-screen ">
       <Navbar />
+      <Routes>
+        <Route path="/" element={<Home coins={coins} />} />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/acccount" element={<Account />} />
+      </Routes>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,13 +2,17 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    colors: {
-      "crypto-blue-light": "#18194B",
-      "crypto-purple-light": "#242450",
-      "crypto-purple-button": "#6C61FF",
-      "crypto-grey": "#262626",
-      "crypto-black": "#131212",
-      "crypto-white": "#FEFEFF",
+    extend: {
+      colors: {
+        "red": "#dc2626",
+        "green": "#16a34a",
+        "crypto-blue-light": "#18194B",
+        "crypto-purple-light": "#242450",
+        "crypto-purple-button": "#6C61FF",
+        "crypto-grey": "#262626",
+        "crypto-black": "#131212",
+        "crypto-white": "#FEFEFF",
+      },
     },
   },
   plugins: [],
