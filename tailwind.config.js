7de77631570fd86e4c5249fdb4af7cc716fbff57
Dylan <dylan.cobham@yahoo.com.au>
2023-05-21 20:41:40 +0930

    fix: react code cleanup and tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    colors: {
+      "crypto-green-light": "#01FEC3",
+      "crypto-green-dark": "#249C91",
+      "crypto-green-grey": "#141618",
+      "crypto-green-black": "#090808",
+    },
+  },
+  plugins: [],
+};
